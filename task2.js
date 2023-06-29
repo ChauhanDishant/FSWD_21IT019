f5ca67e6d3a56c743edb2f8895dd03bf36deb292
ChauhanDishant <98459830+ChauhanDishant@users.noreply.github.com>
2023-06-29 14:41:04 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,18 @@
+function Add(num1, num2) {
+    return num1 + num2;
+}
+function Sub(num1, num2) {
+    return num1 - num2;
+}
+function Mul(num1, num2) {
+    return num1 * num2;
+}
+function Div(num1, num2) {
+    return num1 / num2;
+}
+let num1 = 3
+let num2 = 5
+console.log("The addition of two numbers is : ", Add(num1, num2));
+console.log("The substraction of two numbers is : ", Sub(num1, num2));
+console.log("The multiplication of two numbers is : ", Mul(num1, num2));
+console.log("The division of two numbers is : ", Div(num1, num2));
